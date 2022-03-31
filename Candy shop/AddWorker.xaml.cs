e8c17e113ea17319d66c2d93be79ace3ae100a35
Владимир Борковский <vovaborkovskij@dnevnik.ru>
2022-03-31 23:42:36 +0700

    орпр

--- Candy shop/AddWorker.xaml.cs
+++ Candy shop/AddWorker.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Candy_shop
 {
@@ -19,6 +10,11 @@ namespace Candy_shop
     /// </summary>
     public partial class AddWorker : Window
     {
+        //List
+        //List<char> Alphabetic = new List<char>() { 'а', '' };
+        //string
+        static public string uniqueCode = string.Empty;
+
         public AddWorker()
         {
             InitializeComponent();
@@ -36,9 +32,57 @@ namespace Candy_shop
 
         private void AddWorkerButton_Click(object sender, RoutedEventArgs e)
         {
-            GetCode getCode = new GetCode();
-            getCode.Show();
-            Close();
+            //проверка на заполнение полей
+            if (StringNotEmpty(lNameTextBox.Text) && StringNotEmpty(fNameTextBox.Text) && StringNotEmpty(mNameTextBox.Text)
+                && StringNotEmpty(postComboBox.Text))
+            {
+                //проверка на заглавную букву
+                if (FirstLetterIsLarge(lNameTextBox.Text) && FirstLetterIsLarge(fNameTextBox.Text) && FirstLetterIsLarge(mNameTextBox.Text))
+                {
+                    //создаем уникальный код
+                    uniqueCode = postComboBox.Text[0].ToString();
+
+                    // TODO: надо проверять БД
+                    Random rnd = new Random();
+                    int rand = rnd.Next(0, 1000);
+                    if (rand.ToString().Length == 1)
+                    {
+                        uniqueCode += "—00" + rand.ToString();
+                    }
+                    else if (rand.ToString().Length == 2)
+                    {
+                        uniqueCode += "—0" + rand.ToString();
+                    }
+                    else
+                    {
+                        uniqueCode += "—" + rand.ToString();
+                    }
+
+                    GetCode getCode = new GetCode();
+                    getCode.Show();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Поля в ФИО должны начинаться в заглавной буквы");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Все обязательные поля должны быть заполнены");
+            }
+        }
+
+        //функция проверки на заполнение
+        private bool StringNotEmpty(string _str)
+        {
+            return _str != string.Empty && _str != "" && _str.Trim() != "" ? true : false;
+        }
+
+        //функция проверки на заглавную первую букву
+        private bool FirstLetterIsLarge(string _str)
+        {
+            return Regex.IsMatch(_str[0].ToString(), @"[А-Я]");
         }
     }
 }
