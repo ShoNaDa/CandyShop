f1199c2a05f8e23972e34e18eb6ebbfafe1d954d
Владимир Борковский <vovaborkovskij@dnevnik.ru>
2022-03-31 13:46:07 +0700

    Добавьте файлы проекта.

---
+++ Candy shop/AddWorker.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Candy_shop
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWorker.xaml
+    /// </summary>
+    public partial class AddWorker : Window
+    {
+        public AddWorker()
+        {
+            InitializeComponent();
+        }
+
+        private void editPhotoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancalButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddWorkerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Candy shop/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Candy_shop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddWorker addWorker = new AddWorker();
+            addWorker.Show();
+            Close();
+        }
+    }
+}
